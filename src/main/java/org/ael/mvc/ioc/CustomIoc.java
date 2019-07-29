e2f25839e34ee2e4f266011534964283999739d3
aorxsr <9624344i>
2019-07-29 22:47:38 +0800

    2019.07.29 IOC 开始编写

---
+++ src/main/java/org/ael/mvc/ioc/CustomIoc.java
@@ -0,0 +1,13 @@
+package org.ael.mvc.ioc;
+
+import org.ael.mvc.ioc.bean.Bean;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CustomIoc {
+
+    private Map<String, Bean> beans = new ConcurrentHashMap<>(32);
+
+
+}
---
+++ src/main/java/org/ael/mvc/ioc/bean/Bean.java
@@ -0,0 +1,57 @@
+package org.ael.mvc.ioc.bean;
+
+public class Bean {
+
+    private Object bean;
+    private Class<?> type;
+    private boolean singleton;
+    private boolean fieldHasPrototype;
+
+    public Bean(Object bean) {
+        this(bean, bean.getClass());
+    }
+
+    public Bean(Object bean, Class<?> type) {
+        this.bean = bean;
+        this.type = type;
+        this.singleton = true;
+    }
+
+    public Bean(Object bean, Class<?> type, boolean isSingleton) {
+        this.bean = bean;
+        this.type = type;
+        this.singleton = isSingleton;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+
+    public void setBean(Object bean) {
+        this.bean = bean;
+    }
+
+    public Class<?> getType() {
+        return type;
+    }
+
+    public void setType(Class<?> type) {
+        this.type = type;
+    }
+
+    public boolean isSingleton() {
+        return singleton;
+    }
+
+    public void setSingleton(boolean isSingleton) {
+        this.singleton = isSingleton;
+    }
+
+    public boolean isFieldHasPrototype() {
+        return fieldHasPrototype;
+    }
+
+    public void setFieldHasPrototype(boolean fieldHasPrototype) {
+        this.fieldHasPrototype = fieldHasPrototype;
+    }
+}
