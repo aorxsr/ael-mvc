674f73628ee31dc5a2a5f0653ad0952dd174c74f
aorxsr <861499095@qq.com>
2019-08-24 15:21:00 +0800

    删除无用的引用

--- src/main/java/org/ael/mvc/http/session/SessionManager.java
+++ src/main/java/org/ael/mvc/http/session/SessionManager.java
@@ -1,9 +1,8 @@
 package org.ael.mvc.http.session;
 
 import lombok.Getter;
-import org.ael.mvc.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.ael.mvc.http.Session;
-import sun.plugin2.message.Message;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -16,47 +15,48 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Author: aorxsr
  * @Date: 2019/7/29 17:28
  */
+@Slf4j
 public class SessionManager {
 
-    MessageDigest md = null;
+	MessageDigest md = null;
 
-    @Getter
-    private Map<String, Session> sessionMap = new ConcurrentHashMap<>();
+	@Getter
+	private Map<String, Session> sessionMap = new ConcurrentHashMap<>();
 
-    public Session putSession(Session session) {
-        sessionMap.put(session.getId(), session);
-        return session;
-    }
+	public Session putSession(Session session) {
+		sessionMap.put(session.getId(), session);
+		return session;
+	}
 
-    public synchronized String generateId() {
-        try {
-            md = MessageDigest.getInstance("MD5");
-            UUID uuid = UUID.randomUUID();
-            String guidStr = uuid.toString();
-            md.update(guidStr.getBytes(), 0, guidStr.length());
-            String sessionId = new BigInteger(1, md.digest()).toString(16);
+	public synchronized String generateId() {
+		try {
+			md = MessageDigest.getInstance("MD5");
+			UUID uuid = UUID.randomUUID();
+			String guidStr = uuid.toString();
+			md.update(guidStr.getBytes(), 0, guidStr.length());
+			String sessionId = new BigInteger(1, md.digest()).toString(16);
 
-            if (sessionMap.containsKey(sessionId)) {
-                return generateId();
-            } else {
-                return sessionId;
-            }
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Generate SessionId Fail...");
-            return generateId();
-        }
-    }
+			if (sessionMap.containsKey(sessionId)) {
+				return generateId();
+			} else {
+				return sessionId;
+			}
+		} catch (NoSuchAlgorithmException e) {
+			log.error("Generate SessionId Fail...");
+			return generateId();
+		}
+	}
 
-    public Session getSession(String sessionId) {
-        return sessionMap.get(sessionId);
-    }
+	public Session getSession(String sessionId) {
+		return sessionMap.get(sessionId);
+	}
 
-    public void destructionSession(String id) {
-        sessionMap.remove(id);
-    }
+	public void destructionSession(String id) {
+		sessionMap.remove(id);
+	}
 
-    public void destructionSession(Session session) {
-        sessionMap.remove(session.getId());
-    }
+	public void destructionSession(Session session) {
+		sessionMap.remove(session.getId());
+	}
 
 }
